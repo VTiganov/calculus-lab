b43d00f4fcab1c18e7d1a068743a1d62ddd5f909
Tiganov Vadim <vadimyxy@gmail.com>
2025-01-11 00:20:52 +0300

    dichotomy

--- src/main/java/com/example/Task1.java
+++ src/main/java/com/example/Task1.java
@@ -1,5 +1,29 @@
 package com.example;
 
 public class Task1 {
+    public static float func(float x){
+        return x;
+    }
+
+    public static float dichotomyMethod(float a, float b, float tolerance){
+        float c;
+        while ((b - a) / 2.0 > tolerance) {
+            c = (a + b) / 2;
+            
+            if(func(c) == 0){
+                return c;
+            }
+
+            else if(func(c) * func(a) < 0){
+                b = c;
+            }
+
+            else{
+                a = c;
+            }
+        }
+        return (a + b) / 2;
+    }
+
     
 }
